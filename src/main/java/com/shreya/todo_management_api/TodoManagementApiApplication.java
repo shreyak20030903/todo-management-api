5690f7997dae4eaa4578fed50719f5f0c03b603b
shreyak20030903 <shreyakushwaha1173@gmail.com>
2026-07-19 02:26:05 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/shreya/todo_management_api/TodoManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.shreya.todo_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagementApiApplication.class, args);
+	}
+
+}
